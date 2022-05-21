508e6f22e4112c435c9b4a72515238bc0c886cfb
AnjanaLP <anjana.lpn@gmail.com>
2022-05-21 14:00:52 +0100

    refactor cypress tests

--- cypress/integration/ShoppingList.spec.js
+++ cypress/integration/ShoppingList.spec.js
@@ -1,29 +1,33 @@
 describe("Shopping List", function() {
-  it("shows a heading", function() {
+  beforeEach(() => {
     cy.visit('/');
+  })
+
+  it("shows a heading", function() {
     cy.get('#heading').should("contain", "Shopping List");
   });
 
   it("can add and show an item", function() {
-    cy.visit('/');
-    cy.get('#item-textbox').type("Milk");
-    cy.get('#addItem-button').click();
+    addMilk();
     cy.get('#item-0').should("contain", "Milk");
   });
 
   it("can toggle a line through an item via a checkbox", function() {
-    cy.visit('/');
-    cy.get('#item-textbox').type("Milk");
-    cy.get('#addItem-button').click();
+    addMilk();
     cy.get('#item-0-checkbox').click();
     cy.get('#item-0').should('have.css', 'text-decoration', 'line-through solid rgb(0, 0, 0)');
   });
 
   it("can clear all the items", function() {
-    cy.visit('/');
-    cy.get('#item-textbox').type("Milk");
-    cy.get('#addItem-button').click();
+    addMilk();
+    addMilk();
     cy.get('#shoppingList-clearLink').click();
     cy.get('#item-0').should('not.exist');
+    cy.get('#item-1').should('not.exist');
   });
+
+  const addMilk = () => {
+    cy.get('#item-textbox').type("Milk");
+    cy.get('#addItem-button').click();
+  }
 });
